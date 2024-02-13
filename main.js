1179846cbefb67963d8ed4fa29bc180b507d4b21
rasuljon <anvarovr87@gmail.com>
2024-02-13 14:55:33 +0300

    add index.html

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-let a = +prompt("istagan soningizni kiriting")
-let b = +prompt("qayta son kiriting")
-let c = +prompt("yana br bor kiriting")
+let userName = prompt("ismingizni kiriting")
+let userAge = +prompt(userName + " yoshingizni kiriting")
+
+if (userAge >= "0" && userAge <= "5" ) {
+    alert("boxchaga bor");
+}else if(userAge >= "6" && userAge <= "18"){
+    alert("maytap darsini qil")
+}else if(userAge >= "19" && userAge <= "25"){
+    alert("yaxshi ishlaga joylashvoldinmi")
+}else if(userAge >= "26"  && userAge <= "40"){
+    alert("ish haqqi kamliq qivottimi?")
+}else if(userAge >= "41" && userAge <= "55"){
+    alert("tabrik yaqinda pensaga ciqas")
+}else {
+    alert("pensaga chiqadigan vohtin bopti")
+}
+
+let a = +prompt("bopti " + userName +" istagan sonini kirit")
+let b = +prompt("qayta son kirit")
+let c = +prompt("yana br bor kirit")
 
 if (a < b && a > c || a < c&& a > b) {
     console.log(a);
